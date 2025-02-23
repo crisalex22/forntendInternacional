a045e0972dfd09456e80f01ed72d86b74fe56b13
crisalex22 <60363924+crisalex22@users.noreply.github.com>
2025-02-23 00:18:26 -0500

    chore: first commit creacion del proyecto base

---
+++ Ejercicio-Internacional/src/main/java/com/Internacional/Ejercicio/Internacional/EjercicioInternacionalApplication.java
@@ -0,0 +1,13 @@
+package com.Internacional.Ejercicio.Internacional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioInternacionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioInternacionalApplication.class, args);
+	}
+
+}
