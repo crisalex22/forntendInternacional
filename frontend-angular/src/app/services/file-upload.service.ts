5426f7215dd6971a38f3b1ce000aba9df8205a87
crisalex22 <60363924+crisalex22@users.noreply.github.com>
2025-02-22 21:59:34 -0500

    feat: creacion del servicio para conectarse al backend

---
+++ frontend-angular/src/app/services/file-upload.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileUploadService {
+  private apiUrl = 'http://localhost:8080/upload'; // URL del backend
+
+  constructor(private http: HttpClient) {}
+
+  uploadFile(file: File): Observable<any> {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    return this.http.post(this.apiUrl, formData);
+  }
+}
